7c4c350fad0962e83ad1e9e853d0d3a06dd47d99
AnneLineC <anneline.col@gmail.com>
2023-09-21 11:18:59 +0200

    Feat: Register page - fields added - useStates

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,50 @@
+import { Button, FormControl, TextField, Typography } from '@mui/material';
+import { useState } from 'react';
+
 function Register() {
+    const [email, setEmail] = useState('');
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmationPassword, setConfirmationPassword] = useState('');
+
     return (
         <>
-            <h1>Register</h1>
+            <Typography>Bienvenue !</Typography>
+            <Typography component="h1">Créer un compte</Typography>
+            <FormControl>
+                <TextField
+                    label="Adresse email"
+                    variant="outlined"
+                    type="email"
+                    required
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                />
+                <TextField
+                    label="Nom d'utilisateur"
+                    variant="outlined"
+                    required
+                    value={userName}
+                    onChange={(event) => setUserName(event.target.value)}
+                />
+                <TextField
+                    label="Mot de passe"
+                    variant="outlined"
+                    type="password"
+                    required
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                />
+                <TextField
+                    label="Confirmation du mot de passe"
+                    variant="outlined"
+                    type="password"
+                    required
+                    value={confirmationPassword}
+                    onChange={(event) => setConfirmationPassword(event.target.value)}
+                />
+                <Button variant="contained">Envoyer</Button>
+            </FormControl>
         </>
     );
 }
