c707d948f7893bdc90d03241e7de7a41d37a16d7
AnneLineC <anneline.col@gmail.com>
2023-09-21 15:31:09 +0200

    Feat: Register form error handling - password rules checked - confirmation password checked Pair-programming with Mathieu F.

--- src/client-side/pages/Register.tsx
+++ src/client-side/pages/Register.tsx
@@ -11,24 +11,49 @@ function Register() {
     const [userName, setUserName] = useState('');
     const [password, setPassword] = useState('');
     const [confirmationPassword, setConfirmationPassword] = useState('');
+    const [isPasswordWrong, setIsPasswordWrong] = useState(false);
+    const [wrongConfirmationPasswordMessage, setWrongConfirmationPasswordMessage] = useState('');
+
     const dispatch = useAppDispatch();
+
+    const isConfirmationPasswordEqual = (password: string, confirmationPassword: string) => {
+        return password === confirmationPassword;
+    };
+
+    const isValidPassword = () => {
+        const passwordRegex = /^(?=(?:.*\d))(?=(?:.*[a-zA-Z]))(?=(?:.*[A-Z])).{12,}$/;
+        return passwordRegex.test(password);
+    };
 
     const onSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        console.log('Submit');
-        const userInput: UserInput = {
-            username: userName,
-            email: email,
-            password: password,
-        };
-        dispatch(registerUser({ userInterface: dependencyContainer.get<UserApi>('UserApi'), userInput: userInput }));
+
+        if (isValidPassword()) {
+            setIsPasswordWrong(false);
+
+            if (isConfirmationPasswordEqual(password, confirmationPassword)) {
+                setWrongConfirmationPasswordMessage('');
+                const userInput: UserInput = {
+                    username: userName,
+                    email: email,
+                    password: password,
+                };
+                dispatch(
+                    registerUser({ userInterface: dependencyContainer.get<UserApi>('UserApi'), userInput: userInput }),
+                );
+            } else {
+                setWrongConfirmationPasswordMessage('Les mots de passe ne correspondent pas');
+            }
+        } else {
+            setIsPasswordWrong(true);
+        }
     };
 
     return (
         <>
             <Typography>Bienvenue !</Typography>
             <Typography component="h1">Créer un compte</Typography>
-            <form onSubmit={onSubmitHandler}>
+            <form onSubmit={onSubmitHandler} style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
                 <TextField
                     label="Adresse email"
                     variant="outlined"
@@ -48,17 +73,21 @@ function Register() {
                     label="Mot de passe"
                     variant="outlined"
                     type="password"
+                    error={isPasswordWrong}
                     required
                     value={password}
                     onChange={(event) => setPassword(event.target.value)}
+                    helperText="Le mot de passe doit faire au moins 12 caractères, doit contenir au moins une majuscule, un chiffre et un caractère spécial."
                 />
                 <TextField
                     label="Confirmation du mot de passe"
                     variant="outlined"
                     type="password"
+                    error={wrongConfirmationPasswordMessage !== ''}
                     required
                     value={confirmationPassword}
                     onChange={(event) => setConfirmationPassword(event.target.value)}
+                    helperText={wrongConfirmationPasswordMessage}
                 />
                 <Button type="submit" variant="contained">
                     Envoyer
