a36c8cf9d045c2b8339ce048696aeeb1e0b9fc8e
AnneLineC <anneline.col@gmail.com>
2023-09-21 10:43:22 +0200

    Feature: react-router-dom - library installed - homepage and register routes created with corresponding pages Pair-prog with Mathieu F.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/Home.tsx';
+import Register from './pages/Register.tsx';
+import { ThemeProvider } from '@mui/material';
+import { theme } from './theme.ts';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <HomePage />,
+    },
+
+    {
+        path: '/inscription',
+        element: <Register />,
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <App />
+            <RouterProvider router={router} />
     </React.StrictMode>,
 );
