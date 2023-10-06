c8c16275cf39e8b30468c5983f33f91681a87416
Mathieu Bonhommeau <m.bonhommeau@unlimitedpix.com>
2023-10-06 08:53:56 +0200

    add deploy job

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: parseFloat(process.env.LISTEN_PORT)
+  },
+  preview: {
+    port: parseFloat(process.env.LISTEN_PORT)
+  }
 })
