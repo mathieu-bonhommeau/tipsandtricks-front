5efc6001015b5737618906ed60699730b337c7ed
AnneLineC <anneline.col@gmail.com>
2023-10-10 11:10:25 +0200

    dotenv process removal

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: parseFloat(process.env.LISTEN_PORT)
-  },
-  preview: {
-    port: parseFloat(process.env.LISTEN_PORT)
-  }
-})
+    plugins: [react()],
+});
